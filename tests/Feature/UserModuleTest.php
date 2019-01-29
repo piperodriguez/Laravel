314787d218e13e28a2e2060ab80c1a8be68ec37b
piperodriguez <vargasjuan367@gmail.com>
2019-01-28 20:07:38 -0500

    Leccion 5 Controllers Con arreglo de pruebas

---
+++ tests/Feature/UserModuleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserModuleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    function testValidacionTextoRuta()
+    {
+    	/*permite saber que esa ruta esta corriendo exitosamente osea en estado 200*/
+        $this->get('/usuarios')
+        	->assertStatus(200)
+        	->assertSee('Usuarios');
+        	/*ademas comprueba que si esta retornando el texto Usuarios*/
+
+    }
+
+
+}
