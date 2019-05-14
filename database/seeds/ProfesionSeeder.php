0eb0286b593006ab3d5effac624a67e7f49a515a
Juan Felipe <frodriguez@solati.com.co>
2019-05-14 11:47:25 -0400

    Descripcion: repaso leccion13 consultasSQl

--- database/seeds/ProfesionSeeder.php
+++ database/seeds/ProfesionSeeder.php
@@ -11,12 +11,14 @@ class ProfesionSeeder extends Seeder
      * @return void
      */
     public function run()
-    {	
+    {
 
     	date_default_timezone_set('America/Bogota');
     	$created_at = date("Y-m-d H:i:s");
+        /*Forma 1*/
+        //DB::insert('INSERT INTO profesiones (titulo, created_at) VALUES (?, ?)', ['Back-End Developer',$created_at]);
 
-
+        /*FORMA DE REALIZAR UN INSERT 2*/
        DB::table('profesiones')->insert([
         	'titulo' => 'Back-End Developer',
         	'created_at' => $created_at
@@ -42,5 +44,13 @@ class ProfesionSeeder extends Seeder
         	'created_at' => $created_at
         ]);
 
+
+        /*FORMA 3*/
+        /*DB::insert('INSERT INTO profesiones (titulo, created_at)
+                    VALUES (:title, :fechaCreate)',
+                    [
+                        'title' =>'Back-End Developer',
+                        'fechaCreate' => $created_at
+                    ]);*/
     }
 }
--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -12,14 +12,18 @@ class UsersSeeder extends Seeder
     public function run()
     {
 
+
+      //$Profesion = DB::select('SELECT id_profesion FROM profesiones WHERE titulo = "Back-End Developer"');
+
+
+      //dd($Profesion);
+
        $propfesion = DB::table('profesiones')
        ->select('id_profesion')
        ->where(['titulo' => 'Back-End Developer'])
        ->value('id_profesion');
 
        /*/dd($propfesion->first()->id_profesion);//$profesion[0]*/
-
-
 
     	date_default_timezone_set('America/Bogota');
     	$created_at = date("Y-m-d H:i:s");
